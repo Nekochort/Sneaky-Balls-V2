019b8762149e2ac7b8519e964c26facf92180a4a
skrininshot <38591071+skrininshot@users.noreply.github.com>
2022-07-21 21:34:44 +0500

    Pespawn Fix

--- Assets/Scripts/Game/DeadPlatform.cs
+++ Assets/Scripts/Game/DeadPlatform.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class DeadPlatform : MonoBehaviour
 {
-    public GameObject respawnPlayer;
+    private GameObject respawnPlayer;
     public GameObject[] spawns;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Player")
         {
-            respawnPlayer = GameObject.FindGameObjectWithTag("Player");
-            Vector3 randomPosition = spawns[Random.Range(0, spawns.Length)].transform.localPosition;
+            respawnPlayer = collision.gameObject;
+            Vector3 randomPosition = spawns[0].transform.localPosition;
             respawnPlayer.transform.position = randomPosition;
         }
     }
